55f528fd34641dcf983f2bed1f8308de05ce37e8
Co Tran <tranbinhco99@gmail,com>
2023-04-18 22:59:49 +0700

    load list coaches from firebase and create reload action

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -6,13 +6,16 @@ export default {
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
-      areas: data.areas
+      areas: data.areas,
     };
 
-    const response = await fetch(`https://devcoaches-b9996-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`, {
-      method: 'PUT',
-      body: JSON.stringify(coachData)
-    });
+    const response = await fetch(
+      `https://devcoaches-b9996-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
 
     // const responseData = await response.json();
 
@@ -22,7 +25,30 @@ export default {
 
     context.commit('registerCoach', {
       ...coachData,
-      id: userId
+      id: userId,
     });
-  }
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://devcoaches-b9996-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      //to do handle error
+    }
+
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
+  },
 };
